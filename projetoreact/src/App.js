6c6500aaa37293805881507cfc79108b6245ea1a
Lucas Fernandes <lucasferrnandes10@gmail.com>
2023-01-10 01:06:50 +0000

    form criado

---
+++ projetoreact/src/App.js
@@ -0,0 +1,14 @@
+import Form from "./components/Form/form";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Form />
+        <h1> Colé </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
